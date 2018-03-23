c15848a55d257302af6fde8acf6d9dcd3b96a852
Unknown <s.dellaoui@gmail.com>
2018-03-23 10:24:19 +0100

    Add Character text display
    DIsplays left or right avatar depending on the last displayed text

--- AudioBookApp/Assets/Scripts/Content/ContentController.cs
+++ AudioBookApp/Assets/Scripts/Content/ContentController.cs
@@ -2,23 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CharacterTextItem
+{
+    CharacterText_Left,
+    CharacterText_Right,
+};
+
 public class ContentController : MonoBehaviour {
 
 	public GameObject contentPanel;
 	public GameObject itemPrefab;
+    private bool characterText_Left = true;
 	// Use this for initialization
 	void Start () {
-
+        /*
+        bool characterText_Left = true;
+        //string itemToSelect;
+        CharacterTextItem itemToSelect;
 		for (int i = 0; i < 10; i++) {
-			GameObject item = Instantiate (itemPrefab) as GameObject;
-			item.transform.SetParent(contentPanel.transform, false);
-			//item.transform.parent = contentPanel.transform;
-		}
+            itemToSelect = (characterText_Left) ? CharacterTextItem.CharacterText_Left : CharacterTextItem.CharacterText_Right;
 
+            GameObject item = Instantiate (Resources.Load("00_Prefabs/"+itemToSelect.ToString(), typeof(GameObject))) as GameObject;
+			item.transform.SetParent(contentPanel.transform, false);
+            characterText_Left = !characterText_Left;
+
+        }
+        */
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            DisplayNewCharacterDialog();
+        }
 	}
+
+    void DisplayNewCharacterDialog()
+    {
+        CharacterTextItem itemToSelect;
+        itemToSelect = (characterText_Left) ? CharacterTextItem.CharacterText_Left : CharacterTextItem.CharacterText_Right;
+        GameObject item = Instantiate(Resources.Load("00_Prefabs/" + itemToSelect.ToString(), typeof(GameObject))) as GameObject;
+        item.transform.SetParent(contentPanel.transform, false);
+        characterText_Left = !characterText_Left;
+    }
 }
