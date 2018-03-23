bf97f460fae841b7102901c6e57b90d4ccd74d4a
Unknown <s.dellaoui@gmail.com>
2018-03-24 00:54:47 +0100

    Add Fade on appear item

--- AudioBookApp/Assets/Scripts/Content/ContentController.cs
+++ AudioBookApp/Assets/Scripts/Content/ContentController.cs
@@ -12,30 +12,65 @@ public enum CharacterTextItem
 public class ContentController : MonoBehaviour {
 
 	public GameObject contentPanel;
-	public GameObject itemPrefab;
-    private bool characterText_Left = true;
-	// Use this for initialization
-	void Start () {
+    public GameObject contentViewPort;
+    public float displayFadeinTime;
 
+    private bool characterText_Left = true;
+    private ScrollRect contentScrollRect;
+    // Use this for initialization
+    void Awake()
+    {
+        contentScrollRect = contentViewPort.GetComponent<ScrollRect>();
+    }
+	void Start () {
+        if (displayFadeinTime == 0f)
+            displayFadeinTime += 0.01f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            DisplayNewCharacterDialog();
-        }
 	}
 
     public void DisplayNewCharacterDialog()
     {
         CharacterTextItem itemToSelect;
         itemToSelect = (characterText_Left) ? CharacterTextItem.CharacterText_Left : CharacterTextItem.CharacterText_Right;
-        GameObject item = Instantiate(Resources.Load("00_Prefabs/" + itemToSelect.ToString(), typeof(GameObject))) as GameObject;       
+        GameObject item = Instantiate(Resources.Load("00_Prefabs/" + itemToSelect.ToString(), typeof(GameObject))) as GameObject;
         item.transform.SetParent(contentPanel.transform, false);
         LayoutRebuilder.ForceRebuildLayoutImmediate(contentPanel.GetComponent<RectTransform>());
+        StartCoroutine("DisplayCharacterDialog", item);
         characterText_Left = !characterText_Left;
+    }
+    IEnumerator DisplayCharacterDialog(GameObject item)
+    {
+        CanvasGroup itemCanvasGroup = item.GetComponent<CanvasGroup>();
+        float elapsedTime = 0f;
+        float scrollPosition = contentScrollRect.verticalNormalizedPosition;
+        while (itemCanvasGroup.alpha < 1)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForSeconds(0.01f);
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime/displayFadeinTime);
+            itemCanvasGroup.alpha = (float)System.Math.Round(alpha, 2);
+            float scrollValue = Mathf.Lerp(scrollPosition, 0, elapsedTime / displayFadeinTime);
+            contentScrollRect.verticalNormalizedPosition = scrollValue;
+        }
+        StartCoroutine("ScrollToBottom");
+        yield return null;
+    }
+    IEnumerator ScrollToBottom()
+    {
+        float elapsedTime = 0f;
+        float scrollPosition = contentScrollRect.verticalNormalizedPosition;
+        while (scrollPosition > 0)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForSeconds(0.05f);
+            float scrollValue = Mathf.Lerp(scrollPosition, 0, elapsedTime / (displayFadeinTime*4));
+            contentScrollRect.verticalNormalizedPosition = scrollValue;
+        }
+        Debug.Log("Fini");
+        yield return null;
     }
 }
